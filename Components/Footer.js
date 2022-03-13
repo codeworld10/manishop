ff30911ae899791dbc8fd6f104aafc6d31bbcffa
Muhammad Usman <manijnb0333@gmail.com>
2022-03-13 18:28:46 +0500

    completed

---
+++ Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div className='ft'>
+       <div className='fttext'>Designed And Developed By maniwebdev</div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
